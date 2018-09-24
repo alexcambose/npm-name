dfb7698a223eab8b8f38f5d3fb02a0930adc263a
Alexandru Cambose <alexcambose@yahoo.com>
2018-09-24 20:22:53 +0300

    UI imporvements

--- script.js
+++ script.js
@@ -6,19 +6,6 @@ const npmPackage = Vue.component('npm-package', {
     from: String,
     onlyAvailable: Boolean,
   },
-  data: function() {
-    return {
-      loading: true,
-      avalability: null,
-      rawInfo: {},
-      modalInfoOpened: false,
-    };
-  },
-  computed: {
-    info() {
-      return this.rawInfo;
-    },
-  },
   mounted: async function() {
     try {
       let res = await fetch(
@@ -36,9 +23,47 @@ const npmPackage = Vue.component('npm-package', {
       this.rawInfo = res;
     } catch (e) {
       this.avalability = true;
-      console.log('aer');
     }
     this.loading = false;
+  },
+  data: function() {
+    return {
+      loading: true,
+      avalability: null,
+      rawInfo: {},
+      modalInfoOpened: false,
+      clicked: false,
+    };
+  },
+  computed: {
+    info() {
+      return this.rawInfo;
+    },
+    borderOpacity() {
+      const { score } = this;
+      if (score > 7) return 1;
+      else if (score > 3) return 0.5;
+      else if (score > 1) return 0.3;
+      return 0.1;
+    },
+    borderWidth() {
+      const { score } = this;
+
+      if (score > 10) return 2;
+      if (score < 1) return 0;
+      return 1;
+    },
+  },
+  methods: {
+    clickAvailble() {
+      if (this.avalability) {
+        copyToClipboard(this.word);
+        this.clicked = !this.clicked;
+        if (this.clicked) {
+          setTimeout(() => (this.clicked = false), 1000);
+        }
+      }
+    },
   },
   name: 'npm-package',
   template: '#npm-package',
@@ -71,7 +96,8 @@ const app = new Vue({
       }
       this.fetchingWords = true;
       fetch(
-        `http://cors-anywhere.herokuapp.com/http://relatedwords.org/api/related?term=${word}`
+        `http://cors-anywhere.herokuapp.com/http://relatedwords.org/api/related?term=${word}`,
+        { origin: null }
       )
         .then(e => e.json())
         .then(e => {
@@ -81,10 +107,32 @@ const app = new Vue({
     }, 500),
     loadMore: function() {
       this.skip += Math.min(LOAD_AMOUNT, this.allWords.length);
-      console.log('loaded more', this.skip);
     },
   },
   components: {
     npmPackage,
   },
 });
+
+// Copy to clipboard, from https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+
+const copyToClipboard = str => {
+  const el = document.createElement('textarea'); // Create a <textarea> element
+  el.value = str; // Set its value to the string that you want copied
+  el.setAttribute('readonly', ''); // Make it readonly to be tamper-proof
+  el.style.position = 'absolute';
+  el.style.left = '-9999px'; // Move outside the screen to make it invisible
+  document.body.appendChild(el); // Append the <textarea> element to the HTML document
+  const selected =
+    document.getSelection().rangeCount > 0 // Check if there is any content selected previously
+      ? document.getSelection().getRangeAt(0) // Store selection if found
+      : false; // Mark as false to know no selection existed before
+  el.select(); // Select the <textarea> content
+  document.execCommand('copy'); // Copy - only works as a result of a user action (e.g. click events)
+  document.body.removeChild(el); // Remove the <textarea> element
+  if (selected) {
+    // If a selection existed before copying
+    document.getSelection().removeAllRanges(); // Unselect everything on the HTML document
+    document.getSelection().addRange(selected); // Restore the original selection
+  }
+};
